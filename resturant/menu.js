d937c20ca29073cdf67ed38388e1b067e6da5c43
poojakundala633 <122975055+poojakundala633@users.noreply.github.com>
2023-02-28 10:41:55 +0530

    Add files via upload

---
+++ resturant/menu.js
@@ -0,0 +1,28 @@
+const menu = {
+    item1: {
+      name:"Peri Fries",
+      cost: 350,
+      category : "Starter",
+    },
+    item2: {
+      name: "French Fries",
+      cost: 150,
+      category: "Starter",
+    },
+    item3: {
+      name: "Home Country Fries with Herbs and Chilli Flakes",
+      cost: 150,
+      category: "Starter",
+    },
+    item4: {
+      name: "GulabJamun",
+      cost: 200,
+      category: "Desert",
+    },
+    item5: {
+      name: "French Fries with Cheese and Jalapenons",
+      cost: 300,
+      category: "Main Course",
+    },
+    
+};
